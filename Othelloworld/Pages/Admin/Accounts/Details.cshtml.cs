cbfb41e1ff2dffbf2c2aa1ddc5a8b9c48d93dadf
Patrick Wobben <patrickwobben@live.nl>
2022-10-28 08:32:10 +0200

    Change Role in Details

--- Othelloworld/Pages/Admin/Accounts/Details.cshtml.cs
+++ Othelloworld/Pages/Admin/Accounts/Details.cshtml.cs
@@ -3,7 +3,9 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Othelloworld.Data.Models;
 using System.Collections.Generic;
+using System.Data;
 using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Othelloworld.Pages.Admin.Accounts
@@ -22,16 +24,28 @@ namespace Othelloworld.Pages.Admin.Accounts
 
 		[BindProperty]
 		public Account Account { get; set; }
+
+		[BindProperty]
+		public string Role { get; set; }
+
+		[BindProperty]
+		public string NewRole { get; set; }
+		
 		public async Task OnGetAsync(string username)
 		{
 			Account = await _userManager.FindByNameAsync(username);
+			Role = (await _userManager.GetRolesAsync(Account)).First();
 		}
-
+		
 		public async Task<IActionResult> OnPostAsync()
 		{
-			var result = await _userManager.UpdateAsync(Account);
+			var OldRole = (await _userManager.GetRolesAsync(Account)).First();
 
-			if(result.Succeeded)
+			var result = await _userManager.UpdateAsync(Account);
+			var roleRemoveResult = await _userManager.RemoveFromRoleAsync(Account, OldRole);
+			var roleAddResult = await _userManager.AddToRoleAsync(Account, NewRole);
+
+			if(result.Succeeded )
 			{
 				return Page();
 			} else
