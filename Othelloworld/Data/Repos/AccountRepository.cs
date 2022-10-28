982a2a803d41fe2eb30d428844486f07513ab49f
PW\pw <patrickwobben@live.nl>
2022-10-28 09:11:43 +0200

    Acc repo

---
+++ Othelloworld/Data/Repos/AccountRepository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Othelloworld.Data.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace Othelloworld.Data.Repos
+{
+	public class AccountRepository : IAccountRepository
+	{
+		private OthelloDbContext _context;
+		private UserManager<Account> _userManager;
+
+		public AccountRepository(OthelloDbContext context, UserManager<Account> userManager) 
+		{ 
+			_context = context;
+			_userManager = userManager;
+		}
+
+		public async Task<bool> ReassignRoleAsync(Account account, string currentRole, string newRole)
+		{
+			using (var identitydbContextTransaction = _context.Database.BeginTransaction())
+			{
+				try
+				{
+					var removeResult = await _userManager.RemoveFromRoleAsync(account, currentRole);
+					if (removeResult.Succeeded)
+					{
+						var addResult = await _userManager.AddToRoleAsync(account, newRole);
+
+						if (addResult.Succeeded)
+						{
+							identitydbContextTransaction.Commit();
+							return true;
+						}
+					}
+				}
+				catch (Exception)
+				{
+					identitydbContextTransaction.Rollback();
+					throw;
+				}
+			}
+
+			return false;
+		}
+	}
+}
---
+++ Othelloworld/Data/Repos/IAccountRepository.cs
@@ -0,0 +1,10 @@
+﻿using Othelloworld.Data.Models;
+using System.Threading.Tasks;
+
+namespace Othelloworld.Data.Repos
+{
+	public interface IAccountRepository
+	{
+		public Task<bool> ReassignRoleAsync(Account account, string currentRole, string newRole);
+	}
+}
